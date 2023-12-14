33a6429c398b2b5159106a4c36c96bf66a516d32
Anant Narayan <anantnrg@gmail.com>
2023-12-14 08:14:23 +0530

    fixed fucked up formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,22 @@
 pub mod helpers;
 
 use std::sync::Arc;
+use wgpu::util::DeviceExt;
 
 /// Main struct for Photon
 pub struct Photon {
-	/// Device
-	pub device: Arc<wgpu::Device>,
-	/// Queue
-	pub queue: Arc<wgpu::Queue>,
-	/// Render pipeline
-	pub render_pipeline: wgpu::RenderPipeline,
+    /// Device
+    pub device: Arc<wgpu::Device>,
+    /// Queue
+    pub queue: Arc<wgpu::Queue>,
+    /// Render pipeline
+    pub render_pipeline: wgpu::RenderPipeline,
 }
 
 #[derive(Debug)]
 pub enum PixelOrient {
-	Vertical,
-	Horizontal
+    Vertical,
+    Horizontal,
 }
 
 /// Struct for pixel values representing coordinates in pixels.
@@ -23,8 +24,8 @@ pub enum PixelOrient {
 /// Used to represent x and y coordinates in pixels.
 #[derive(Debug)]
 pub struct Pixel {
-	pub value: f32,
-	pub orient: PixelOrient
+    pub value: f32,
+    pub orient: PixelOrient,
 }
 
 /// Enum representing different types of shapes.
@@ -51,57 +52,52 @@ pub struct Shape {
     pub color: [f32; 4],
 }
 
-
 impl Pixel {
-	/// Constructor method to create a new Pixel instance.
-	///
-	/// # Arguments
-	///
-	/// * `value` - The value in pixels.
-	/// * `orient` - The orientation (height or width).
-	///
-	/// # Example
-	///
-	/// ```
-	/// use photon::Pixel;
-	///
-	/// let pixel = Pixel::new(10.0, PixelOrient::Horizontal);
-	/// ```
-	pub fn new(value: f32, orient: PixelOrient) -> Self {
-		Pixel { value, orient }
-	}
+    /// Constructor method to create a new Pixel instance.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` - The value in pixels.
+    /// * `orient` - The orientation (height or width).
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use photon::Pixel;
+    ///
+    /// let pixel = Pixel::new(10.0, PixelOrient::Horizontal);
+    /// ```
+    pub fn new(value: f32, orient: PixelOrient) -> Self {
+        Pixel { value, orient }
+    }
 
-	/// Method to convert Pixel coordinates to Normalized Device Coordinates (NDC).
-	///
-	/// Converts the pixel coordinates to NDC using the provided window size.
-	///
-	/// # Arguments
-	///
-	/// * `window_size` - A tuple representing the window size (width, height) in pixels.
-	///
-	/// # Returns
-	///
-	/// An f32 value of the NDC coordinate.
-	///
-	/// # Example
-	///
-	/// ```
-	/// use photon::Pixel;
-	///
-	/// let pixel = Pixel::new(100.0, PixelOrient::Horizontal);
-	/// let window_size = (800.0, 600.0);
-	/// let ndc_pixel = pixel.to_ndc(window_size);
-	/// ```
-	pub fn to_ndc(&self, window_size: (f32, f32)) -> f32 {
-		match self.orient {
-			PixelOrient::Horizontal => {
-				(self.value * 2.0) / window_size.0 - 1.0
-			}
-			PixelOrient::Vertical => {
-				(self.value * 2.0) / window_size.1
-			}
-		}
-	}
+    /// Method to convert Pixel coordinates to Normalized Device Coordinates (NDC).
+    ///
+    /// Converts the pixel coordinates to NDC using the provided window size.
+    ///
+    /// # Arguments
+    ///
+    /// * `window_size` - A tuple representing the window size (width, height) in pixels.
+    ///
+    /// # Returns
+    ///
+    /// An f32 value of the NDC coordinate.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use photon::Pixel;
+    ///
+    /// let pixel = Pixel::new(100.0, PixelOrient::Horizontal);
+    /// let window_size = (800.0, 600.0);
+    /// let ndc_pixel = pixel.to_ndc(window_size);
+    /// ```
+    pub fn to_ndc(&self, window_size: (f32, f32)) -> f32 {
+        match self.orient {
+            PixelOrient::Horizontal => (self.value * 2.0) / window_size.0 - 1.0,
+            PixelOrient::Vertical => (self.value * 2.0) / window_size.1,
+        }
+    }
 }
 
 impl Shape {
@@ -155,90 +151,98 @@ impl Shape {
 }
 
 impl Photon {
-	/// Creates a new instance of `Photon`.
-	///
-	/// # Arguments
-	///
-	/// * `device` - Arc-wrapped wgpu::Device for rendering.
-	/// * `queue` - Arc-wrapped wgpu::Queue for rendering commands.
-	/// * `texture_format` - wgpu::TextureFormat used in rendering.
-	///
-	/// # Returns
-	///
-	/// A new `Photon` instance.
-	pub fn new(
-		device: Arc<wgpu::Device>,
-		queue: Arc<wgpu::Queue>,
-		texture_format: wgpu::TextureFormat,
-	) -> Self {
-		let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+    /// Creates a new instance of `Photon`.
+    ///
+    /// # Arguments
+    ///
+    /// * `device` - Arc-wrapped wgpu::Device for rendering.
+    /// * `queue` - Arc-wrapped wgpu::Queue for rendering commands.
+    /// * `texture_format` - wgpu::TextureFormat used in rendering.
+    ///
+    /// # Returns
+    ///
+    /// A new `Photon` instance.
+    pub fn new(
+        device: Arc<wgpu::Device>,
+        queue: Arc<wgpu::Queue>,
+        texture_format: wgpu::TextureFormat,
+    ) -> Self {
+        let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
 
-		let render_pipeline_layout =
-			device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-				label: Some("render_pipeline_layout"),
-				bind_group_layouts: &[],
-				push_constant_ranges: &[],
-			});
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("render_pipeline_layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
 
-		let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-			label: Some("render_pipeline"),
-			layout: Some(&render_pipeline_layout),
-			vertex: wgpu::VertexState { module: &shader, entry_point: "vs_main", buffers: &[] },
-			fragment: Some(wgpu::FragmentState {
-				module: &shader,
-				entry_point: "fs_main",
-				targets: &[Some(wgpu::ColorTargetState {
-					format: texture_format,
-					blend: Some(wgpu::BlendState {
-						color: wgpu::BlendComponent::REPLACE,
-						alpha: wgpu::BlendComponent::REPLACE,
-					}),
-					write_mask: wgpu::ColorWrites::ALL,
-				})],
-			}),
-			primitive: wgpu::PrimitiveState {
-				topology: wgpu::PrimitiveTopology::TriangleList,
-				strip_index_format: None,
-				front_face: wgpu::FrontFace::Ccw,
-				cull_mode: Some(wgpu::Face::Back),
-				polygon_mode: wgpu::PolygonMode::Fill,
-				unclipped_depth: false,
-				conservative: false,
-			},
-			depth_stencil: None,
-			multisample: wgpu::MultisampleState {
-				count: 1,
-				mask: !0,
-				alpha_to_coverage_enabled: false,
-			},
-			multiview: None,
-		});
-		Self { device, queue, render_pipeline }
-	}
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("render_pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                buffers: &[],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: texture_format,
+                    blend: Some(wgpu::BlendState {
+                        color: wgpu::BlendComponent::REPLACE,
+                        alpha: wgpu::BlendComponent::REPLACE,
+                    }),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                polygon_mode: wgpu::PolygonMode::Fill,
+                unclipped_depth: false,
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None,
+        });
+        Self {
+            device,
+            queue,
+            render_pipeline,
+        }
+    }
 
-	/// Encodes rendering commands based on the provided render pass descriptor.
-	///
-	/// # Arguments
-	///
-	/// * `render_pass_desc` - A reference to a wgpu::RenderPassDescriptor.
-	///
-	/// This method sets up rendering commands based on the provided descriptor
-	/// and submits them to the rendering queue.
-	pub fn render_encode(&mut self, render_pass_desc: &wgpu::RenderPassDescriptor) {
-		let device = &self.device;
-		let queue = &self.queue;
+    /// Encodes rendering commands based on the provided render pass descriptor.
+    ///
+    /// # Arguments
+    ///
+    /// * `render_pass_desc` - A reference to a wgpu::RenderPassDescriptor.
+    ///
+    /// This method sets up rendering commands based on the provided descriptor
+    /// and submits them to the rendering queue.
+    pub fn render_encode(&mut self, render_pass_desc: &wgpu::RenderPassDescriptor) {
+        let device = &self.device;
+        let queue = &self.queue;
 
-		let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-			label: Some("render_encoder"),
-		});
+        let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("render_encoder"),
+        });
 
-		let mut render_pass = encoder.begin_render_pass(render_pass_desc);
+        let mut render_pass = encoder.begin_render_pass(render_pass_desc);
 
-		render_pass.set_pipeline(&self.render_pipeline);
+        render_pass.set_pipeline(&self.render_pipeline);
 
-		render_pass.draw(0..3, 0..1);
+        render_pass.draw(0..3, 0..1);
 
-		drop(render_pass);
-		queue.submit(Some(encoder.finish()));
-	}
+        drop(render_pass);
+        queue.submit(Some(encoder.finish()));
+    }
 }
